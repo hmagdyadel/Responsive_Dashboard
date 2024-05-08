4ef5b537cb1298521175c6aa28ce29a8685332bd
Haitham Magdy <hmagdyadel@gmail.com>
2024-05-08 21:46:12 +0200

    finish project

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class CustomTextFeild extends StatelessWidget {
+  const CustomTextFeild({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
